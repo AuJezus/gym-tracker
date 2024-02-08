f82fb7ade24e7f43a2e31913985cb0fb85beac52
aujezus <augispay@gmail.com>
2024-02-08 09:00:23 +0200

    Add save and cancel buttons

--- app/app/workout/create/page.js
+++ app/app/workout/create/page.js
@@ -1,11 +1,27 @@
 "use client";
 
 import ExerciseAddModal from "@/components/exercise-add-modal";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@radix-ui/react-label";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Link from "next/link";
 
 function WorkoutCreatePage() {
   const [sets, setSets] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [isSaving, setIsSaving] = useState(false);
+  const disableSave = isSaving || sets.length < 1 || !name;
 
   const addNewSet = (exercise) => {
     setSets((sets) => [...sets, { exercise, reps: 0, weight: 0 }]);
@@ -37,26 +53,73 @@ function WorkoutCreatePage() {
     });
   };
 
+  const saveWorkout = async () => {
+    const supabase = createClientComponentClient();
+
+    const workout = {
+      name,
+      description,
+    };
+
+    const { data: workoutData, error: workoutErr } = await supabase
+      .from("workouts")
+      .insert(workout)
+      .select()
+      .limit(1)
+      .single();
+    if (workoutErr) console.error(workoutErr);
+
+    const workoutSets = sets.map((set) => ({
+      workout_id: workoutData.id,
+      exercise_id: set.exercise.id,
+      reps: Number(set.reps),
+      weight: Number(set.weight),
+    }));
+
+    const { error: setsError } = await supabase
+      .from("workout-sets")
+      .insert(workoutSets);
+    if (setsError) console.error(setsError);
+  };
+
   return (
     <>
+      <div className="flex items-center gap-4">
+        <Label>Name: </Label>
+        <Input
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center gap-4">
+        <Label>Description: </Label>
+        <Textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
       <div className="divide-y-2">
         {sets.map((set, index) => (
           <div
-            className="grid w-1/2 grid-cols-3 justify-around gap-4 py-2"
+            className="grid w-1/2 grid-cols-3 items-center justify-around gap-4 py-2"
             key={index}
           >
             <p>{set.exercise.name}</p>
             <div className="flex items-center gap-2">
-              <p>Reps: </p>
+              <Label htmlFor={`reps-${index}`}>Reps: </Label>
               <Input
+                id={`reps-${index}`}
                 value={set.reps}
                 onChange={(e) => setReps(index, e.target.value)}
                 type="number"
               />
             </div>
             <div className="flex items-center gap-2">
-              <p>Weight: </p>
+              <Label htmlFor={`weight-${index}`}>Weight: </Label>
               <Input
+                id={`weight-${index}`}
                 value={set.weight}
                 onChange={(e) => setWeight(index, e.target.value)}
                 type="number"
@@ -69,8 +132,29 @@ function WorkoutCreatePage() {
             +
           </button>
         )}
+        <ExerciseAddModal onAdd={addNewSet} />
       </div>
-      <ExerciseAddModal onAdd={addNewSet} />
+      <Button disabled={disableSave} onClick={saveWorkout} type="submit">
+        Save
+      </Button>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="destructive">Cancel</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+          </DialogHeader>
+          <Link
+            href="/app"
+            className={buttonVariants({ variant: "destructive" })}
+          >
+            Absolutely!
+          </Link>
+          <Button variant="outline">Stay here</Button>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
